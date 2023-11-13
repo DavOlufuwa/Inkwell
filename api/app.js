b378aad3c854e9924c1ddf95469c1b55618ba1c2
Dav.O <davolufuwa@gmail.com>
2023-11-13 23:36:07 +0100

    nodemon works

---
+++ api/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+require("express-async-errors");
+require("events").EventEmitter.defaultMaxListeners = 15;
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.set("strictQuery", false);
+
+
+app.use(express.json());
+app.use(cors());
+
+
+module.exports = app;
