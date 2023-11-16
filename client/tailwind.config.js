8e671c9a4f7cab15d14da6de874dced8e2649c70
Dav.O <davolufuwa@gmail.com>
2023-11-16 18:16:01 +0100

    tailwind styles

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{jsx,js,tsx,ts}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "t-light": "#1A1A1A",
+        "t-dark": "#F2F2F2",
+        "c-light": "#667085",
+        "c-dark": "#C0C5D0",
+        "d-light": "#7D5FC5",
+        "d-dark": "#9685BF",
+        "bg-dark": "#090D1F",
+        "bg-light": "#FAFAFA",
+      }
+    },
   },
   plugins: [],
-}
+};
 
