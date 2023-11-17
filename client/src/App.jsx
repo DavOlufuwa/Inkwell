100b48b6c57558e91b54f69ee873ee14dbd9bb7d
Dav.O <davolufuwa@gmail.com>
2023-11-18 00:27:28 +0100

    created the blog card

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
+import Blogcard from "./components/Blogcard"
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1 className="">Samples</h1>
+      <Blogcard />
     </div>
   )
 }
